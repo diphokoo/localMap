2b7ff0a87c3d9f0ebc14db29b81c271cc0e68c20
Obakeng Freddy Diphoko <42438019+diphokoo@users.noreply.github.com>
2024-09-05 13:54:59 +0200

    project files

---
+++ src/Map/LocalMap.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function LocalMap() {
+    return (
+        <div>
+            <h1>
+                Test
+            </h1>
+        </div>
+    )
+}
+
+export default LocalMap;
